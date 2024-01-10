751d688af87d68f75a307fe49c5cd38ed192fa32
shashhhw <shashwatmjn@gmail.com>
2024-01-10 15:04:55 -0500

    Removed normalizing from training scripts

--- scripts/cnn/training.py
+++ scripts/cnn/training.py
@@ -5,14 +5,6 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 
 # Define necessary functions
-# Define function to normalize the displacement and force dataset
-def normalize_data(data_to_be_normalized, training_mean, training_range):
-
-    normalized_data = (data_to_be_normalized - training_mean) / training_range
-
-    return normalized_data
-
-
 # Function to save files
 def save_file(values, file_name, file_directory):
     
@@ -99,58 +91,26 @@ def main():
     # Define directories
     current_directory = os.getcwd()
 
-    # Define directory for the assembled data
-    assembled_data_directory = os.path.join(current_directory, 'data', 'assembled')
+    # Define directory for the normalized data
+    normalized_data_directory = os.path.join(current_directory, 'data', 'normalized')
 
     # Define directory for the trained results
     trained_results_directory = os.path.join(current_directory, 'cnn', 'training_results')
 
     # Working with just the displacement data to conserve memory
-    # Load the training subsets for displacement data
-    print('Loading the training subsets for displacement data...')
-    training_displacement_data = np.load(os.path.join(assembled_data_directory, 'training_displacement_data.npy'))
+    # Load the normalized training subsets for displacement data
+    print('Loading the normalized training subsets for displacement data...')
+    normalized_training_displacement_data = np.load(os.path.join(normalized_data_directory, 'normalized_training_displacement_data.npy'))
 
     # Print the shapes of the displacement data
-    print(f'The shape of displacement data is {training_displacement_data.shape[1:]}.')
+    print(f'The shape of displacement data is {normalized_training_displacement_data.shape[1:]}.')
 
-    # Get normalizing parameters for displacement
-    displacement_mean = np.mean(training_displacement_data)
-    displacement_range = np.max(training_displacement_data) - np.min(training_displacement_data)
-
-    # Normalize the displacement values
-    print('Normalizing the displacement data...')
-    normalized_training_displacement_data = normalize_data(training_displacement_data, displacement_mean, displacement_range)
-
-    # Clear the variables to free up memory
-    del training_displacement_data
-
-    # Load the training subsets for force data
-    print('Loading the training subsets for force data...')
-    training_force_data = np.load(os.path.join(assembled_data_directory, 'training_force_data.npy'))
+    # Load the normalized training subsets for force data
+    print('Loading the normalized training subsets for force data...')
+    normalized_training_force_data = np.load(os.path.join(normalized_data_directory, 'normalized_training_force_data.npy'))
 
     # Print the shapes of the force data
-    print(f'The shape of force data is {training_force_data.shape[1:]}.')
-
-    # Get normalizing parameters for force
-    force_mean = np.mean(training_force_data)
-    force_range = np.max(training_force_data) - np.min(training_force_data)
-
-    # Normalize the force values
-    print('Normalizing the force data...')
-    normalized_training_force_data = normalize_data(training_force_data, force_mean, force_range)
-    
-    # Clear the variables to free up memory
-    del training_force_data
-
-    # Save the normalizing parameters as a .npy file
-    # Bundle everything into a single dictionary
-    normalizing_parameters = {'displacement_mean': displacement_mean,
-                            'displacement_range': displacement_range,
-                            'force_mean': force_mean,
-                            'force_range': force_range}
-
-    # Save the normalizing parameters as a .npy file
-    save_file(normalizing_parameters, 'normalizing_parameters.npy', trained_results_directory)
+    print(f'The shape of force data is {normalized_training_force_data.shape[1:]}.')
 
     # Define variables that remain constant during the training
     input_shape = normalized_training_displacement_data.shape[1:]
